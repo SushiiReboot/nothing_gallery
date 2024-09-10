e143ea555e613120d451d09b73a67c0ca3e41206
Sushi <chverenkool@gmail.com>
2024-09-10 17:12:49 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:nothing_gallery/screens/albums_screen.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const AlbumsScreen(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
